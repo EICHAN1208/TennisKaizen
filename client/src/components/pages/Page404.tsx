826f28a4524a7e79f8f506c3d13b520c249dc670
murakami <ek.h1.hy@gmail.com>
2021-07-24 23:26:00 +0900

    page404の表示

---
+++ client/src/components/pages/Page404.tsx
@@ -0,0 +1,5 @@
+export const Page404 = () => {
+  return(
+    <p>ページ404です</p>
+  )
+}
--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,8 +1,10 @@
 import { Switch, Route } from "react-router-dom";
+
 import { Login } from "../components/pages/Login";
 import { Chart } from "../components/pages/Chart";
 import { Record } from "../components/pages/Record";
 import { AddCategory } from "../components/pages/AddCategory";
+import { Page404 } from "../components/pages/Page404";
 
 export const Router = () => {
   return (
@@ -18,6 +20,9 @@ export const Router = () => {
       </Route>
       <Route path="/new_category">
         <AddCategory />
+      </Route>
+      <Route path="*">
+        <Page404 />
       </Route>
     </Switch>
   )
