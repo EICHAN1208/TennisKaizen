d94042500672d570f661efd746a763ec2a85c0fa
murakami <ek.h1.hy@gmail.com>
2021-07-24 23:25:38 +0900

    カテゴリの名前を返却

--- server/app/controllers/categories_controller.rb
+++ server/app/controllers/categories_controller.rb
@@ -1,4 +1,9 @@
 class CategoriesController < ApplicationController
+  def index
+    categories = Category.all.pluck(:name)
+    render json: { category_names: categories }
+  end
+
   def create
     Category.create!(permitted_params)
   end
